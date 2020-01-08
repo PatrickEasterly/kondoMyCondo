a8733063b039d1750df15f2e382fe3518e664515
Patrick <pateast678@gmail.com>
2020-01-08 11:06:18 -0500

    adds form page, get and post verbs that dont do anything

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 // npm i express body-parser morgan helmet express-es6-template-engine
 // npm i -D nodemon
 
+const http = require('http');
+const express = require('express')
+const app = express();
+
+const PORT = 3000; 
+const server = http.createServer(app);
+
+const morgan = require('morgan');
+const logger = morgan('tiny');
+app.use(logger);
+
+const helmet = require('helmet');
+app.use(helmet());
+
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+const parseForm = bodyParser.urlencoded({
+    extended: true
+});
+
+app.get('/', (req, res) => {
+    res.send('so many dependencies. <a href="/create">create</a>');
+});
+
+app.get('/create', (req, res)=> {
+    res.render('form');
+});
+
+
+server.listen(PORT, ()=> {
+    console.log(`Listening on port ${PORT}...`)
+})
